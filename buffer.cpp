498d7af5aad814084db4920d7fd3095a5d4ac1ad
cyd2333 <37024452+cyd2333@users.noreply.github.com>
2018-11-05 18:56:15 -0600

    Update buffer.cpp

--- buffer.cpp
+++ buffer.cpp
@@ -95,16 +95,69 @@ void BufMgr::allocBuf(FrameId & frame)
 	
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
+    FrameId temp;
+    // look up file and pageid in the hashtable
+    try {
+        hashTable.lookup(file, pageNo, &temp);
+    } catch(HashNotFoundException e1) {
+        // if it is not in the buffer pool
+        allocBuf(clokhand);
+        // read page from file and insert it into buffer pool
+        bufPool[clockHand] = (*file).readPage(pageNo);
+        // insert it into hashtable
+        hashTable.insert(file, pageNo, clockHand);
+        // invoke set()
+        bufDescTable[clockHand].Set(file, pageNo);
+        page = &（bufPool[clockHand]）;
+        return;
+    }
+    // if it is in the buffer pool
+    bufDescTable[temp].refbit = true;
+    bufDescTable[temp].pinCnt ++;
+    page = &(bufPool[temp]);
 }
 
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
+    // check whether this page is in the hashtable
+    try {
+        hashTable.lookup(file, pageNo, &temp);
+    } catch(HashNotFoundException e1) {
+        // if this page is not in the hash table
+        return;
+    }
+    // if this page is in the hash table
+    bufDesc b = bufDescTable[temp];
+    if (b.pinCnt == 0)
+        throw PageNotPinnedException;
+    else {
+        // if this page's pin is bigger than zero
+        b.pinCnt = b.pinCnt - 1;
+        if(dirty == true)
+            b.dirty = true;
+    }
 }
 
 void BufMgr::flushFile(const File* file) 
 {
-
+    // go through the frame array
+    for(int i = 0; i < numBufs; i++) {
+        BufDesc temp = bufDescTable[i];
+        // check whether this frame's page belong to the given file
+        if(temp.file == file) {
+            if(temp.dirty == true) {
+                // flush the page into the disk if it is dirty
+                // and 
+                (*file).writePage(bufPool[i]);
+                temp.dirty = false;
+            }
+            // remove page from the hashtable
+            hashTable.remove(file,tmep.pageNo);
+            // clear the bufdesc
+            temp.Clear();
+        }
+    }
 }
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
